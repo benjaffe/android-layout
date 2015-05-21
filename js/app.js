8a28e13eb6f7f33f16fde0a10b2b19d05c1d8331
Ben Jaffe <ben@udacity.com>
2015-05-20 18:08:10 -0700

    disable line wrapping

--- js/app.js
+++ js/app.js
@@ -68,7 +68,6 @@ var app = app || {};
 			value: app.getCodeForHash(),
 			mode: "android-xml",
 			lineNumbers: true,
-	        lineWrapping: true,
 			indentUnit: 4,
 			fixedGutter: true,
 			viewportMargin: Infinity,
